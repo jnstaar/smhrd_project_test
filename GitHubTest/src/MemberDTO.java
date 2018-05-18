b7e2792b51e0ba9a8ab991b9e706b6c2fccee502
smhrc06 <smhrc06@DESKTOP-O2GK7L6>
2018-05-18 11:42:47 +0900

    아옳옳

--- GitHubTest/src/MemberDTO.java
+++ GitHubTest/src/MemberDTO.java
@@ -1,4 +1,41 @@
 
 public class MemberDTO {
+	private String name;
+	private int age;
+	private String tel;
+	private String memNum;
+	public MemberDTO(String name, int age, String tel, String memNum) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.tel = tel;
+		this.memNum = memNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getMemNum() {
+		return memNum;
+	}
+	public void setMemNum(String memNum) {
+		this.memNum = memNum;
+	}
+	
+	
 
 }
